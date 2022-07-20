5dcddcce7973b31175ace834fe54d2c3c59f8d7c
darwyn57 <93914904+darwyn57@users.noreply.github.com>
2022-07-20 14:38:13 +0200

    mise a jour

--- routes/meme.routes.js
+++ routes/meme.routes.js
@@ -1,6 +1,21 @@
 module.exports = (app) => {// Définition des routes
     const memeController = require('../controllers/meme.controller.js'); // Import du controller
 
+    const multer = require('multer');// Import du module multer
+// On défini la façon dont l'image sera stockée
+
+const storage = multer.diskStorage({
+    destination: './public/uploads',
+    filename: (requete, fichier, cb) => {
+      cb(
+        null,
+        Date.now() +
+          '_' +
+          fichier.originalname.replace(/[^a-z0-9\.]/gi, '_').toLowerCase(),
+      )
+    },
+  })
+
     app.post("/meme", memeController.create); // Création d'un nouveau meme
 
     app.get("/memes", memeController.findAll); // Récupération de tous les memes
